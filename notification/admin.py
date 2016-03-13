a929d7b2e24f6f2d2c0b79c4faee8dab2bc16614
Song <sooon1g1@gmail.com>
2016-03-13 13:09:46 +0800

    modify all admin.py in addition to baby/admin.py

--- notification/admin.py
+++ notification/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from notification.models import *
 
-admin.site.register(notification_apns)
-admin.site.register(notification_gcm)
+class notification_apnsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'createdat', 'updatedat', 'useragent', 'deviceid', 'user_id')
+
+class notification_gcmAdmin(admin.ModelAdmin):
+    list_display = ('id', 'createdat', 'updatedat', 'useragent', 'deviceid', 'user_id')
+
+admin.site.register(notification_apns, notification_apnsAdmin)
+admin.site.register(notification_gcm, notification_gcmAdmin)
--- produce/admin.py
+++ produce/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from produce.models import *
 
-admin.site.register(produce_type)
-admin.site.register(produce)
-admin.site.register(produce_trade)
+class produce_typeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'createdat', 'updatedat', 'name')
+
+class produceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'createdat', 'updatedat', 'name', 'price', 'img', 'produce_type_id')
+
+class produce_tradeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'createdat', 'updatedat', 'money', 'trade_data', 'allpay_data', 'user_id')
+
+admin.site.register(produce_type, produce_typeAdmin)
+admin.site.register(produce, produceAdmin)
+admin.site.register(produce_trade, produce_tradeAdmin)
