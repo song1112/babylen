2f8fc62a81c8f863161424822fa0106f2ac7ee00
Song <sooon1g1@gmail.com>
2016-03-13 05:12:42 +0000

    Configuration

--- baby_user/admin.py
+++ baby_user/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from baby_user.models import user, user_normal, user_daycarecenter, user_bonne 
 
-admin.site.register(user)
+class userAdmin(admin.ModelAdmin):
+    list_display = ('id', 'createdat', 'updatedat', 'account', 'password', 'auth_parents', 'auth_bonne', 'auth_daycarecenter')
+
+admin.site.register(user, userAdmin)
 admin.site.register(user_normal)
 admin.site.register(user_daycarecenter)
 admin.site.register(user_bonne)
---
+++ babylen/apache/override.py
@@ -0,0 +1,3 @@
+from babylen.settings import *
+DEBUG = False
+ALLOWED_HOSTS = ['52.192.177.242']
